b5f57a2a56a5a3c19983966d0d4667fa0c7a8257
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-09-18 09:27:41 +0200

    working in setup

--- dist/plugin.js
+++ dist/plugin.js
@@ -12,64 +12,50 @@
     define("exportTokens", ["require", "exports"], function (require, exports) {
         "use strict";
         Object.defineProperty(exports, "__esModule", { value: true });
-        const tokenExport = () => {
-            const count = 5;
+        const drawSquares = (count) => {
             const nodes = [];
             for (let i = 0; i < count; i++) {
                 const rect = figma.createRectangle();
                 rect.x = i * 150;
-                rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
+                rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0.25 } }];
                 figma.currentPage.appendChild(rect);
                 nodes.push(rect);
             }
             figma.currentPage.selection = nodes;
             figma.viewport.scrollAndZoomIntoView(nodes);
         };
+        const tokenExport = () => {
+            drawSquares(5);
+            console.log(figma.getLocalPaintStyles()[0]);
+            console.log(figma.getStyleById(figma.getLocalPaintStyles()[0].id).name);
+            console.log(figma.getLocalTextStyles());
+            console.log(figma.getLocalEffectStyles());
+            console.log(figma.getLocalGridStyles());
+        };
         exports.default = tokenExport;
     });
     define("index", ["require", "exports", "exportTokens"], function (require, exports, exportTokens_1) {
         "use strict";
         Object.defineProperty(exports, "__esModule", { value: true });
-        // This plugin will open a modal to prompt the user to enter a number, and
-        // it will then create that many rectangles on the screen.
+        // register the UI 
+        // by default it is hidden
         figma.showUI(__html__, { visible: false });
-        // This file holds the main code for the plugins. It has access to the *document*.
-        // You can access browser APIs in the <script> tag inside "ui.html" which has a
-        // full browser environment (see documentation).
+        // figma.command is the menu item executed from the plugin menu
+        // run different functions depending on the provided command
+        //
+        // EXPORT
+        // exports the design tokens
         if (figma.command === 'export') {
             exportTokens_1.default();
+            // always run closePlugin otherwise the plugin will keep running
             figma.closePlugin();
         }
+        // SETTINGS
+        // settings for the design tokens
         if (figma.command === 'settings') {
             figma.ui.show();
         }
     });
-    // This shows the HTML page in "ui.html".
-    // figma.showUI(__html__);
-    // Calls to "parent.postMessage" from within the HTML page will trigger this
-    // callback. The callback will be passed the "pluginMessage" property of the
-    // posted message.
-    // figma.ui.onmessage = msg => {
-    //   // One way of distinguishing between different types of messages sent from
-    //   // your HTML page is to use an object with a "type" property like this.
-    //   if (msg.type === 'create-rectangles') {
-    //     const nodes: SceneNode[] = [];
-    //     for (let i = 0; i < msg.count; i++) {
-    //       const rect = figma.createRectangle();
-    //       rect.x = i * 150;
-    //       rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-    //       figma.currentPage.appendChild(rect);
-    //       nodes.push(rect);
-    //     }
-    //     figma.currentPage.selection = nodes;
-    //     figma.viewport.scrollAndZoomIntoView(nodes);
-    //   }
-    //   // Make sure to close the plugin when you're done. Otherwise the plugin will
-    //   // keep running, which shows the cancel button at the bottom of the screen.
-    //   figma.closePlugin();
-    // };
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
     
     'marker:resolver';
 
--- src/exportTokens.ts
+++ src/exportTokens.ts
@@ -1,15 +1,23 @@
-const tokenExport = () => {
-  const count = 5
+const drawSquares = (count) => {
   const nodes: SceneNode[] = [];
   for (let i = 0; i < count; i++) {
     const rect = figma.createRectangle();
     rect.x = i * 150;
-    rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+    rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0.25}}];
     figma.currentPage.appendChild(rect);
     nodes.push(rect);
   }
   figma.currentPage.selection = nodes;
   figma.viewport.scrollAndZoomIntoView(nodes);
+}
+
+const tokenExport = () => {
+  drawSquares(5)
+  console.log(figma.getLocalPaintStyles()[0])
+  console.log(figma.getStyleById(figma.getLocalPaintStyles()[0].id).name)
+  console.log(figma.getLocalTextStyles())
+  console.log(figma.getLocalEffectStyles())
+  console.log(figma.getLocalGridStyles())
 }
 
 export default tokenExport
