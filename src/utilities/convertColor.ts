aaf3054c11e8ad2623f548f375cfca985ebc7b1b
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-09-30 09:10:21 +0200

    improve convertColor

--- src/utilities/convertColor.ts
+++ src/utilities/convertColor.ts
@@ -2,21 +2,20 @@ import { ColorRgba } from '../../types/valueTypes'
 import roundWithDecimals from './roundWithDecimals'
 
 export const roundRgba = (rgba: {
-  r: number,
-  g: number,
-  b: number,
-  a?: number,
-  opacity?: number
-}): ColorRgba => ({
+    r: number,
+    g: number,
+    b: number,
+    a?: number,
+  }, opacity?: number): ColorRgba => ({
   r: roundWithDecimals(rgba.r),
   g: roundWithDecimals(rgba.g),
   b: roundWithDecimals(rgba.b),
-  a: roundWithDecimals(rgba.opacity || rgba.a || 1)
+  a: roundWithDecimals(opacity || rgba.a || 1)
 })
 
 export const convertPaintToRgba = (paint): ColorRgba => {
   if (paint.type === 'SOLID' && paint.visible === true) {
-    return roundRgba(paint.color)
+    return roundRgba(paint.color, paint.opacity || null)
   }
   return null
 }
