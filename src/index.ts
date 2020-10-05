cbd05c88e8fdfc97c6889cb702e683fb24b04bbc
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-10-05 10:35:47 +0200

    adding settings ui

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import exportTokens from './exportTokens'
 import buildFigmaData from './utilities/buildFigmaData'
-// register the UI 
-// by default it is hidden
-figma.showUI(__html__, {visible: false})
-// const ui = {
-//   settings: figma.showUI(__uiFiles__.settings, {visible: false}),
-//   utilities: figma.showUI(__uiFiles__.utilities, {visible: false})
-// }
-
+const activateUtilitiesUi = () => {
+  // register the utilities UI 
+  // by default it is hidden
+  // @ts-ignore
+  figma.showUI(__uiFiles__.utilities, { visible: false })
+}
 // figma.command is the menu item executed from the plugin menu
 // run different functions depending on the provided command
 //
 // EXPORT
 // exports the design tokens
 if(figma.command === 'export') {
+  activateUtilitiesUi()
   // construct figma data object
   const figmaData = buildFigmaData(figma)
   // export tokens
@@ -25,19 +24,27 @@ if(figma.command === 'export') {
 // SETTINGS
 // settings for the design tokens
 if(figma.command === 'settings') {
-  // const isTokenFrame = node => node.type === "FRAME" && node.name.trim().toLowerCase().substr(0,7) === '_tokens'
-  // const frames = figma.root.children.map(page => page.findChildren(node => isTokenFrame(node))).reduce((flatten, arr) => [...flatten, ...arr])
-  // figma.ui.show()
+  // register the settings UI
+  // by default it is hidden
+  // @ts-ignore
+  figma.showUI(__uiFiles__.settings, {
+    visible: false,
+    width: 500,
+    height: 220
+  })
+  // @ts-ignore
+  figma.ui.show(__uiFiles__.settings)
 }
 // HELP
 // Open github help page
 if (figma.command === 'help') {
+  activateUtilitiesUi()
   figma.ui.postMessage({
     command: "help"
   })
 }
 
-
+// CLOSE PLUGIN
 figma.ui.onmessage = (message) => {
   if (message.command === 'closePlugin') {
     figma.closePlugin()
