7bcce8e9853e6856b3469a6915d6cc3fc00f54ea
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-11-19 15:41:37 +0100

    add test for extractSizes

---
+++ tests/unit/extractSizes.test.ts
@@ -0,0 +1,59 @@
+import extractSizes from '../../src/extractor/extractSizes'
+import { customTokenNode } from './data/customTokenNode.data'
+
+describe('extracting sizes', () => {
+  const nodeArray = [
+    customTokenNode,
+    { 
+      ...customTokenNode,
+      ...{ name: 'sizes/10' } 
+    },
+    { 
+      ...customTokenNode,
+      ...{ 
+        name: 'sizes/10 no desc',
+        description: null,
+        width: 10.2345,
+        height: .567
+      } 
+    }
+  ]
+
+  test('extracting only the token with correct name from customTokenNodesArray', () => {
+    expect(extractSizes(nodeArray)).toStrictEqual([{
+      category: 'size',
+      description: 'a description text',
+      name: 'sizes/10',
+      values: {
+        height: {
+          type: 'number',
+          unit: 'pixel',
+          value: 20,
+        },
+        width: {
+          type: 'number',
+          unit: 'pixel',
+          value: 10,
+        }
+      }
+    },
+    {
+      category: 'size',
+      description: null,
+      name: 'sizes/10 no desc',
+      values: {
+        height: {
+          type: 'number',
+          unit: 'pixel',
+          value: 0.57,
+        },
+        width: {
+          type: 'number',
+          unit: 'pixel',
+          value: 10.23,
+        }
+      }
+    }
+  ])
+  })
+})
