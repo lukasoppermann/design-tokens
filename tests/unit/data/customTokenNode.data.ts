5e50cba7380d6e300da72fbeb42f8984474b531f
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-11-19 15:43:46 +0100

    add test for extractSizes (#46)

---
+++ tests/unit/data/customTokenNode.data.ts
@@ -0,0 +1,22 @@
+export const customTokenNode = {
+  name: 'notRecognizedName/customTokenNode',
+  // this could be undefined
+  description: 'a description text',
+  // bottomLeftRadius: 3,
+  // bottomRightRadius: 4,
+  // topLeftRadius: 5,
+  // topRightRadius: 6,
+  // cornerRadius: 'mixed',
+  // cornerSmoothing: .35,
+  // strokes: getSolidStrokes(node.strokes),
+  // strokeWeight: node.strokeWeight,
+  // strokeStyleId: node.strokeStyleId,
+  // strokeMiterAngle: node.strokeMiterAngle || undefined,
+  // strokeMiterLimit: node.strokeMiterLimit,
+  // strokeJoin: node.strokeJoin,
+  // strokeCap: node.strokeCap,
+  // dashPattern: node.dashPattern,
+  // strokeAlign: node.strokeAlign,
+  width: 10,
+  height: 20
+}
