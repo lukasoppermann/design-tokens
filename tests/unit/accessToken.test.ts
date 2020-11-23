757e5c845371f40334c6df02b8fef27a4ce82494
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-11-23 13:11:10 +0100

    add tests for accessToken function

---
+++ tests/unit/accessToken.test.ts
@@ -0,0 +1,44 @@
+import {getAccessToken, setAccessToken} from '../../src/utilities/accessToken'
+
+beforeAll(() => {
+  // @ts-ignore
+  global.figma = {
+    clientStorage: {
+      getAsync: jest.fn()
+    }
+  }
+
+})
+
+describe("Testing getAccessToken", () => {
+  test("return correct token object", () => {
+    // @ts-ignore
+    global.figma.clientStorage.getAsync.mockReturnValue(Promise.resolve({
+      '125454sdaf': 'test'
+    }))
+
+    return getAccessToken('125454sdaf').then(data => {
+      expect(data).toBe('test')
+    })
+  })
+
+  test("return string instead of token object", () => {
+    // @ts-ignore
+    global.figma.clientStorage.getAsync.mockReturnValue(Promise.resolve('wrong'))
+
+    return getAccessToken('125454sdaf').then(data => {
+      expect(data).toBe('')
+    })
+  })
+
+  test("token does not exist", () => {
+    // @ts-ignore
+    global.figma.clientStorage.getAsync.mockReturnValue(Promise.resolve({
+      '125454sdaf': 'test'
+    }))
+
+    return getAccessToken('N25454sdaf').then(data => {
+      expect(data).toBe('')
+    })
+  })
+})
