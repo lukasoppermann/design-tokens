fe5ae3ebd173bcbad207c40154b25bd410f67172
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-11-24 21:29:33 +0100

    working on settings test

--- tests/unit/settings.test.ts
+++ tests/unit/settings.test.ts
@@ -3,6 +3,10 @@ import { getSettings, setSettings, __testing } from '../../src/utilities/setting
 beforeAll(() => {
   // @ts-ignore
   global.figma = {
+    root: {
+      getPluginData: jest.fn(),
+      setPluginData: jest.fn()
+    },
     clientStorage: {
       getAsync: jest.fn()
     }
@@ -59,5 +63,47 @@ describe("Testing settingsPrepare", () => {
     expect(__testing.settingsPrepare(newSettings, {
       acceptHeader: 'test'
     })).toStrictEqual(output)
+  })
+})
+
+describe("Testing setSettings", () => {
+  test("add valid settings to empty array", () => {
+    const currentSettings = {
+      filename: '',
+      excludePrefix: false,
+      prefix: '',
+      serverUrl: '',
+      eventType: '',
+      acceptHeader: '',
+      authType: ''
+    }
+    // @ts-ignore
+    figma.root.getPluginData.mockReturnValue(currentSettings)
+
+    const newSettings = {
+      filename: 'myFile',
+      excludePrefix: true,
+      prefix: '#',
+      serverUrl: 'https://test.com',
+      eventType: 'myEvent',
+      acceptHeader: 'yo',
+      authType: 'aType'
+    }
+
+    const output = {
+      filename: 'myFile',
+      excludePrefix: true,
+      prefix: '#',
+      serverUrl: 'https://test.com',
+      eventType: 'myEvent',
+      acceptHeader: 'yo',
+      authType: 'aType'
+    }
+    setSettings(newSettings)
+    // assert
+    // @ts-ignore
+    expect(figma.root.getPluginData).toHaveReturnedWith(currentSettings)
+    // @ts-ignore
+    expect(figma.root.setPluginData).toHaveBeenCalledWith("settings", JSON.stringify(output, null, 2))
   })
 })
