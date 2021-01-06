15bea8a6bbfad77ec659eda06d01b53a764fd24b
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-01-06 09:28:36 +0100

    Spacings token (#61)
    * adding spacing tokens
    
    * add index.ts to linting

--- src/utilities/getTokenJson.ts
+++ src/utilities/getTokenJson.ts
@@ -4,6 +4,7 @@ import extractFonts from '../extractor/extractFonts'
 import extractEffects from '../extractor/extractEffects'
 import extractMotion from '../extractor/extractMotion'
 import extractSizes from '../extractor/extractSizes'
+import extractSpacing from '../extractor/extractSpacing'
 import extractBorders from '../extractor/extractBorders'
 import extractRadii from '../extractor/extractRadii'
 import groupByName from './groupByName'
@@ -19,6 +20,7 @@ const exportRawTokenArray = (figmaData: figmaDataType) => {
   // get tokens
   return [
     ...extractSizes(figmaData.tokenFrames),
+    ...extractSpacing(figmaData.tokenFrames),
     ...extractBorders(figmaData.tokenFrames),
     ...extractRadii(figmaData.tokenFrames),
     ...extractMotion(figmaData.tokenFrames),
---
+++ tests/unit/extractSpacing.test.ts
@@ -0,0 +1,120 @@
+import extractSpacing from '../../src/extractor/extractSpacing'
+import { customTokenNode } from './data/customTokenNode.data'
+
+describe('extracting spacing', () => {
+  const nodeArray = [
+    customTokenNode,
+    { 
+      ...customTokenNode,
+      ...{ 
+        name: 'spacing/padding/0',
+      }
+    },
+    { 
+      ...customTokenNode,
+      ...{ 
+        name: 'spacing/padding/10',
+        paddingTop: 10,
+        paddingRight: 10,
+        paddingBottom: 10,
+        paddingLeft: 10
+      } 
+    },
+    { 
+      ...customTokenNode,
+      ...{ 
+        name: 'spacing/margin/mixed',
+        description: null,
+        paddingTop: 10,
+        paddingRight: 20,
+        paddingBottom: 30,
+        paddingLeft: 40
+      } 
+    }
+  ]
+
+  test('extracting only the token with correct name from customTokenNodesArray', () => {
+    expect(extractSpacing(nodeArray)).toStrictEqual([{
+      category: 'spacing',
+      description: 'a description text',
+      name: 'spacing/padding/0',
+      values: {
+        top: {
+          value: 0,
+          unit: "pixel",
+          type: 'number'
+        },
+        right: {
+          value: 0,
+          unit: "pixel",
+          type: 'number'
+        },
+        bottom: {
+          value: 0,
+          unit: "pixel",
+          type: 'number'
+        },
+        left: {
+          value: 0,
+          unit: "pixel",
+          type: 'number'
+        }
+      }
+    },
+    {
+      category: 'spacing',
+      description: 'a description text',
+      name: 'spacing/padding/10',
+      values: {
+        top: {
+          value: 10,
+          unit: "pixel",
+          type: 'number'
+        },
+        right: {
+          value: 10,
+          unit: "pixel",
+          type: 'number'
+        },
+        bottom: {
+          value: 10,
+          unit: "pixel",
+          type: 'number'
+        },
+        left: {
+          value: 10,
+          unit: "pixel",
+          type: 'number'
+        }
+      }
+    },
+    {
+      category: 'spacing',
+      description: null,
+      name: 'spacing/margin/mixed',
+      values: {
+        top: {
+          value: 10,
+          unit: "pixel",
+          type: 'number'
+        },
+        right: {
+          value: 20,
+          unit: "pixel",
+          type: 'number'
+        },
+        bottom: {
+          value: 30,
+          unit: "pixel",
+          type: 'number'
+        },
+        left: {
+          value: 40,
+          unit: "pixel",
+          type: 'number'
+        }
+      }
+    }
+  ])
+  })
+})
