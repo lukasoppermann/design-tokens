eecbe97e3467aaa771a5f3a09cb9a367a211649b
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-03-25 20:20:16 +0100

    moving config out & improving getVersionDifference

---
+++ src/utilities/config.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+export default {
+  settingsDialog: {
+    width: 550,
+    height: 565
+  },
+  key: {
+    lastVersionSettingsOpened: 'lastVersionSettingsOpened'
+  }
+}
