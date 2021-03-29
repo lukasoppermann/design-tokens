484eb5f305335cb8a12a0700ba7c283b7b369d13
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-03-29 23:02:31 +0200

    fix getJson test

--- tests/unit/getJson.test.ts
+++ tests/unit/getJson.test.ts
@@ -1,5 +1,9 @@
+const getJsonTokenMock = jest.fn()
+jest.mock('../../src/utilities/getTokenJson', () => getJsonTokenMock)
+// eslint-disable-next-line
 import getJson from '../../src/utilities/getJson'
-jest.mock('../../src/utilities/getTokenJson', () => () => [{ test: 'test' }])
+// jest.mock('../../src/utilities/getTokenJson', () => () => [{ test: 'test' }])
+
 jest.mock('../../src/utilities/buildFigmaData', () => () => ({
   tokenFrames: [],
   paintStyles: [],
@@ -8,19 +12,27 @@ jest.mock('../../src/utilities/buildFigmaData', () => () => ({
   effectStyles: []
 }))
 
-const userSettings = {
+let userSettings = {
   nameConversion: 'default',
   prefix: '_',
   excludePrefix: true
 }
 
 describe('getJson', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
   test('stringify true', () => {
+    getJsonTokenMock.mockReturnValue([{ test: 'test' }])
     // @ts-ignore
     expect(getJson({}, userSettings)).toStrictEqual('[{"test":"test"}]')
+    expect(getJsonTokenMock.mock.calls[0][2]).toStrictEqual('default')
   })
   test('stringify false', () => {
+    userSettings.nameConversion = 'camelCase'
+    getJsonTokenMock.mockReturnValue([{ test: 'test' }])
     // @ts-ignore
     expect(getJson({}, userSettings, false)).toStrictEqual([{ test: 'test' }])
+    expect(getJsonTokenMock.mock.calls[0][2]).toStrictEqual('camelCase')
   })
 })
