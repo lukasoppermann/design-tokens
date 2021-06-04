f0956c3ec6d7e30c587f074e67c729a1e4fcd9f3
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-06-04 22:04:46 +0200

    working on converting to react

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env, argv) => ({
   devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
   entry: {
-    ui: './src/ui/ui.ts', // The entry point for your UI code
+    ui: './src/ui/ui.tsx', // The entry point for your UI code
     plugin: './src/index.ts' // The entry point for your plugin code
   },
 
