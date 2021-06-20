c0a05ddf7cda0a658d4cd2b01315bc58f1f9d7fb
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-06-20 19:00:51 +0200

    update to defaults file

--- src/ui/context.tsx
+++ src/ui/context.tsx
@@ -1,41 +1,11 @@
 import * as React from 'react'
-import { Settings } from '../../types/settings'
 
 export const FigmaContext = React.createContext(null)
 
 export const SettingsContext = React.createContext(null)
 
-export const initialSettings: Settings = {
-  filename: 'design-tokens',
-  extension: '.json',
-  nameConversion: 'default',
-  compression: false,
-  prefix: '_',
-  excludePrefix: true,
-  serverUrl: undefined,
-  eventType: 'update-tokens',
-  accessToken: undefined,
-  acceptHeader: 'application/vnd.github.everest-preview+json',
-  authType: 'token',
-  keyInName: true,
-  exports: {
-    color: true,
-    font: true,
-    effect: true,
-    grid: true,
-    border: true,
-    breakpoint: true,
-    radius: true,
-    size: true,
-    spacing: true,
-    motion: true
-  }
-}
-
 export const settingsReducer = (draft, action) => {
   switch (action.type) {
-    case 'reset':
-      return initialSettings
     case 'load': {
       return action.payload
     }
