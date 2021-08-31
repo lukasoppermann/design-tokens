22eeacb57eb3aefe147880f9449c48963e6476b9
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-08-31 11:44:03 +0200

    prepare tests & desired output for standard

---
+++ tests/unit/transformer.standardTransformer.test.ts
@@ -0,0 +1,17 @@
+import { transformer } from '../../src/transformer/originalFormatTransformer'
+import { extractedFigmaTokens } from './data/extractedFigmaTokens.data'
+import { transformedStandardTokens } from './data/transformedStandardTokens.data'
+
+describe('originalFormatTransfomer', () => {
+  test('size token', () => expect(transformer(extractedFigmaTokens.size)).toStrictEqual(transformedStandardTokens.size))
+  test('breakpoint token', () => expect(transformer(extractedFigmaTokens.breakpoint)).toStrictEqual(transformedStandardTokens.breakpoint))
+  test('spacing token', () => expect(transformer(extractedFigmaTokens.spacing)).toStrictEqual(transformedStandardTokens.spacing))
+  test('radius mixed token', () => expect(transformer(extractedFigmaTokens.radiusMixed)).toStrictEqual(transformedStandardTokens.radiusMixed))
+  test('radius single token', () => expect(transformer(extractedFigmaTokens.radiusSingle)).toStrictEqual(transformedStandardTokens.radiusSingle))
+  test('grid token', () => expect(transformer(extractedFigmaTokens.grid)).toStrictEqual(transformedStandardTokens.grid))
+  test('font token', () => expect(transformer(extractedFigmaTokens.font)).toStrictEqual(transformedStandardTokens.font))
+  test('border token', () => expect(transformer(extractedFigmaTokens.border)).toStrictEqual(transformedStandardTokens.border))
+  test('color token', () => expect(transformer(extractedFigmaTokens.color)).toStrictEqual(transformedStandardTokens.color))
+  test('effect token', () => expect(transformer(extractedFigmaTokens.effect)).toStrictEqual(transformedStandardTokens.effect))
+  test('motion token', () => expect(transformer(extractedFigmaTokens.motion)).toStrictEqual(transformedStandardTokens.motion))
+})
