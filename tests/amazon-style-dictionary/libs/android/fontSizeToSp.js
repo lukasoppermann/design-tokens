c2ee3373423ec802001bf023a3cd86122fe86876
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-09-14 14:34:35 +0200

    reset tests

--- tests/amazon-style-dictionary/libs/android/fontSizeToSp.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  type: 'value',
-  matcher: function (token) {
-    return token.type === 'custom-fontStyle'
-  },
-  transformer: function (token) {
-    return `${token.value.fontSize}sp`
-  }
-}
--- tests/amazon-style-dictionary/libs/android/pxToDp.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  type: 'value',
-  matcher: function (token) {
-    return token.type === 'dimension'
-  },
-  transformer: function (token) {
-    return `${token.value}dp`
-  }
-}
