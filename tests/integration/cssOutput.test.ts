725664ecfb4396ebdb3265339ff9dcaa4fd50451
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-09-29 16:04:52 +0200

    fix integration tests

--- tests/integration/cssOutput.test.ts
+++ tests/integration/cssOutput.test.ts
@@ -18,14 +18,14 @@ describe('Compare css converterd file to data set', () => {
 
   test('standard css data', () => {
     // read files
-    let originalCss = fs.readFileSync('./tests/integration/data/standard.variables.css', 'utf8').replace(/^\s+|\s+$/g, '')
+    const css = fs.readFileSync('./tests/integration/data/standard.variables.css', 'utf8').replace(/^\s+|\s+$/g, '')
     // remove starting comment
-    const lines = originalCss.split('\n')
+    const lines = css.split('\n')
     // remove comment from start
     lines.splice(0, lines.findIndex(line => line === ':root {'))
     // join the array back into a single string
-    originalCss = lines.join('\n')
+    const convertedCss = lines.join('\n')
     // compare to data
-    expect(originalCss).toStrictEqual(cssStandardOutputData)
+    expect(convertedCss).toStrictEqual(cssStandardOutputData)
   })
 })
