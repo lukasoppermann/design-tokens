02331f3664c14703bb02e4d43ded3b5f8211cf86
Lukas Oppermann <oppermann.lukas@gmail.com>
2022-06-16 12:29:45 +0200

    update webpack (#210)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = (env, argv) => ({
   mode: argv.mode === 'production' ? 'production' : 'development',
@@ -17,13 +18,23 @@ module.exports = (env, argv) => ({
   module: {
     rules: [
       // Converts TypeScript code to JavaScript
-      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
 
       // Enables including CSS by doing "import './file.css'" in your TypeScript code
-      { test: /\.css$/, loader: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
 
       // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
-      { test: /\.(png|jpg|gif|webp|svg|zip)$/, loader: [{ loader: 'url-loader' }] }
+      {
+        test: /\.(png|jpg|gif|webp|svg|zip)$/,
+        use: 'url-loader'
+      }
     ]
   },
 
@@ -40,12 +51,15 @@ module.exports = (env, argv) => ({
 
   // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
   plugins: [
+    new webpack.DefinePlugin({
+      global: {} // Fix missing symbol error when running in developer VM
+    }),
     new HtmlWebpackPlugin({
+      inject: 'body',
       template: './src/ui/ui.html',
       filename: 'ui.html',
-      inlineSource: '.(js)$',
       chunks: ['ui']
     }),
-    new HtmlWebpackInlineSourcePlugin()
+    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/ui/])
   ]
 })
