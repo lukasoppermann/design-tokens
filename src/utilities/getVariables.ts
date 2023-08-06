4915de7d03124b6a204d19a328c32405aeedd2d0
Ryan Zec <basire@gmail.com>
2023-08-06 12:25:44 -0400

    added proper support for variable alias when transforming tokens using style dictionary

--- src/utilities/getVariables.ts
+++ src/utilities/getVariables.ts
@@ -13,12 +13,18 @@ const extractVariable = (variable, value) => {
   if (value.type === 'VARIABLE_ALIAS') {
     const resolvedAlias = figma.variables.getVariableById(value.id)
     const collection = figma.variables.getVariableCollectionById(resolvedAlias.variableCollectionId)
+
     return {
       name: variable.name,
       description: variable.description || undefined,
       exportKey: tokenTypes.variables.key as tokenExportKeyType,
       category: getVariableTypeByValue(Object.values(resolvedAlias.valuesByMode)[0]),
-      values: `{${collection.name.toLowerCase()}.${changeNotation(resolvedAlias.name, '/', '.')}}`
+      values: `{${collection.name.toLowerCase()}.${changeNotation(resolvedAlias.name, '/', '.')}}`,
+
+      // this is being stored so we can properly update the design tokens later to account for all 
+      // modes when using aliases
+      aliasName: collection.name,
+      aliasModes: collection.modes
     }
   }
   switch (variable.resolvedType) {
@@ -54,6 +60,38 @@ const extractVariable = (variable, value) => {
   }
 }
 
+const processAliasModes = (variables) => {
+  return variables.reduce((collector, variable) => {
+    // nothing needs to be done to variables that has no alias modes
+    if (!variable.aliasModes) {
+      collector.push(variable)
+
+      return collector
+    }
+
+    const aliasModes = variable.aliasModes
+    const aliasName = variable.aliasName
+
+    // this was only added for this function to process that data so before we return the variables, we can remove it
+    delete variable.aliasModes
+    delete variable.selfCollectionName
+
+    for (let i = 0; i < aliasModes.length; i++) {
+      const modeBasedVariable = { ...variable }
+      const nameParts = modeBasedVariable.name.split('/');
+
+      nameParts.splice(1, 0, aliasModes[i].name)
+
+      modeBasedVariable.values = modeBasedVariable.values.replace(`{${aliasName}.`, `{${aliasName}.${aliasModes[i].name}.`)
+      modeBasedVariable.name = nameParts.join('/')
+
+      collector.push(modeBasedVariable)
+    }
+
+    return collector
+  }, [])
+}
+
 export const getVariables = (figma: PluginAPI) => {
   // get collections
   const collections = Object.fromEntries(figma.variables.getLocalVariableCollections().map((collection) => [collection.id, collection]))
@@ -81,5 +119,6 @@ export const getVariables = (figma: PluginAPI) => {
       }
     })
   })
-  return variables.flat()
+
+  return processAliasModes(variables.flat())
 }
