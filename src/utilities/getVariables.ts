e20ccc7b5c97e745c01d9651e4d064179cab684d
Jack Howard <jack.howard@expel.io>
2024-01-18 12:23:17 -0600

    style: Fix lint

--- src/utilities/getVariables.ts
+++ src/utilities/getVariables.ts
@@ -1,22 +1,22 @@
-import config from "@config/config";
-import { tokenTypes } from "@config/tokenTypes";
-import { tokenCategoryType } from "@typings/tokenCategory";
-import { tokenExportKeyType } from "@typings/tokenExportKey";
-import { PropertyType } from "@typings/valueTypes";
-import { roundRgba } from "./convertColor";
-import { changeNotation } from "./changeNotation";
-import { getVariableTypeByValue } from "./getVariableTypeByValue";
-import roundWithDecimals from "./roundWithDecimals";
-import { Settings } from "@typings/settings";
+import config from '@config/config'
+import { tokenTypes } from '@config/tokenTypes'
+import { tokenCategoryType } from '@typings/tokenCategory'
+import { tokenExportKeyType } from '@typings/tokenExportKey'
+import { PropertyType } from '@typings/valueTypes'
+import { roundRgba } from './convertColor'
+import { changeNotation } from './changeNotation'
+import { getVariableTypeByValue } from './getVariableTypeByValue'
+import roundWithDecimals from './roundWithDecimals'
+import { Settings } from '@typings/settings'
 
 const extractVariable = (variable, value, mode) => {
-  let category: tokenCategoryType = "color";
-  let values = {};
-  if (value.type === "VARIABLE_ALIAS") {
-    const resolvedAlias = figma.variables.getVariableById(value.id);
+  let category: tokenCategoryType = 'color'
+  let values = {}
+  if (value.type === 'VARIABLE_ALIAS') {
+    const resolvedAlias = figma.variables.getVariableById(value.id)
     const collection = figma.variables.getVariableCollectionById(
       resolvedAlias.variableCollectionId
-    );
+    )
     return {
       name: variable.name,
       description: variable.description || undefined,
@@ -26,92 +26,92 @@ const extractVariable = (variable, value, mode) => {
       ),
       values: `{${collection.name.toLowerCase()}.${changeNotation(
         resolvedAlias.name,
-        "/",
-        "."
+        '/',
+        '.'
       )}}`,
 
       // this is being stored so we can properly update the design tokens later to account for all
       // modes when using aliases
       aliasCollectionName: collection.name.toLowerCase(),
-      aliasMode: mode,
-    };
+      aliasMode: mode
+    }
   }
   switch (variable.resolvedType) {
-    case "COLOR":
-      category = "color";
+    case 'COLOR':
+      category = 'color'
       values = {
         fill: {
           value: roundRgba(value),
-          type: "color" as PropertyType,
-          blendMode: "normal",
-        },
-      };
-      break;
-    case "FLOAT":
-      category = "dimension";
-      values = roundWithDecimals(value, 2);
-      break;
-    case "STRING":
-      category = "string";
-      values = value;
-      break;
-    case "BOOLEAN":
-      category = "boolean";
-      values = value;
-      break;
+          type: 'color' as PropertyType,
+          blendMode: 'normal'
+        }
+      }
+      break
+    case 'FLOAT':
+      category = 'dimension'
+      values = roundWithDecimals(value, 2)
+      break
+    case 'STRING':
+      category = 'string'
+      values = value
+      break
+    case 'BOOLEAN':
+      category = 'boolean'
+      values = value
+      break
   }
   return {
     name: variable.name,
     description: variable.description || undefined,
     exportKey: tokenTypes.variables.key as tokenExportKeyType,
     category,
-    values,
-  };
-};
+    values
+  }
+}
 
 const processAliasModes = (variables) => {
   return variables.reduce((collector, variable) => {
     // nothing needs to be done to variables that have no alias modes, or only one mode
     if (!variable.aliasMode) {
-      collector.push(variable);
+      collector.push(variable)
 
-      return collector;
+      return collector
     }
 
-    const { aliasMode, aliasCollectionName } = variable;
+    const { aliasMode, aliasCollectionName } = variable
 
     // this was only added for this function to process that data so before we return the variables, we can remove it
-    delete variable.aliasMode;
-    delete variable.aliasCollectionName;
+    delete variable.aliasMode
+    delete variable.aliasCollectionName
 
     collector.push({
       ...variable,
       values: variable.values.replace(
         `{${aliasCollectionName}.`,
         `{${aliasCollectionName}.${aliasMode.name}.`
-      ),
-    });
+      )
+    })
 
-    return collector;
-  }, []);
-};
+    return collector
+  }, [])
+}
 
 export const getVariables = (figma: PluginAPI, settings: Settings) => {
   const excludedCollectionIds = figma.variables
     .getLocalVariableCollections()
     .filter(
       (collection) =>
-        ![".", "_", ...settings.exclusionPrefix.split(",")].includes(
+        !['.', '_', ...settings.exclusionPrefix.split(',')].includes(
           collection.name.charAt(0)
         )
     )
-    .map((collection) => collection.id);
+    .map((collection) => collection.id)
   // get collections
   const collections = Object.fromEntries(
     figma.variables
       .getLocalVariableCollections()
       .map((collection) => [collection.id, collection])
-  );
+  )
   // get variables
   const variables = figma.variables
     .getLocalVariables()
@@ -120,12 +120,12 @@ export const getVariables = (figma: PluginAPI, settings: Settings) => {
     )
     .map((variable) => {
       // get collection name and modes
-      const { variableCollectionId } = variable;
-      const { name: collection, modes } = collections[variableCollectionId];
+      const { variableCollectionId } = variable
+      const { name: collection, modes } = collections[variableCollectionId]
       // return each mode value as a separate variable
       return Object.entries(variable.valuesByMode).map(([id, value]) => {
         // Only add mode if there's more than one
-        let addMode = settings.modeReference && modes.length > 1;
+        const addMode = settings.modeReference && modes.length > 1
         return {
           ...extractVariable(
             variable,
@@ -148,13 +148,13 @@ export const getVariables = (figma: PluginAPI, settings: Settings) => {
               collection: collection,
               scopes: variable.scopes,
               [config.key.extensionVariableStyleId]: variable.id,
-              exportKey: tokenTypes.variables.key as tokenExportKeyType,
-            },
-          },
-        };
-      });
-    });
+              exportKey: tokenTypes.variables.key as tokenExportKeyType
+            }
+          }
+        }
+      })
+    })
   return settings.modeReference
     ? processAliasModes(variables.flat())
-    : variables.flat();
-};
+    : variables.flat()
+}
